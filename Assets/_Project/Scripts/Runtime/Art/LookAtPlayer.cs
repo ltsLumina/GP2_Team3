9eb84234604b56fb74690b6c933284d344125690
Lumina <alexander.andrejeff@elev.ga.lbs.se>
2025-01-28 01:45:09 +0100

    i was bored at 1am

--- Assets/_Project/Scripts/Runtime/Art/LookAtPlayer.cs
+++ Assets/_Project/Scripts/Runtime/Art/LookAtPlayer.cs
@@ -1,18 +1,44 @@
+using UnityEditor;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class LookAtPlayer : MonoBehaviour
 {
-    private Player player;
+    Player player;
 
+    void Start() => player = FindAnyObjectByType<Player>();
 
-    private void Start()
+#if UNITY_EDITOR
+    void OnEnable()
     {
-        player = FindAnyObjectByType<Player>();
+        if (!Application.isPlaying) EditorApplication.update += Update;
     }
+
+    void OnDisable()
+    {
+        if (!Application.isPlaying) EditorApplication.update -= Update;
+    }
+#endif
 
     // Update is called once per frame
     void Update()
     {
-        transform.up = transform.position - player.transform.position;
+        switch (Application.isPlaying)
+        {
+            case true: // play mode
+                transform.up = transform.position - player.transform.position;
+                break;
+
+            case false when Application.isEditor: // edit mode 
+            {
+                if (SceneView.lastActiveSceneView)
+                {
+                    Camera sceneViewCamera = SceneView.lastActiveSceneView.camera;
+                    transform.up = transform.position - sceneViewCamera.transform.position;
+                }
+
+                break;
+            }
+        }
     }
 }
--- Assets/_Project/Scripts/Runtime/Player/Character.cs
+++ Assets/_Project/Scripts/Runtime/Player/Character.cs
@@ -6,32 +6,45 @@ public class Character : MonoBehaviour
     [SerializeField] float speed = 7.0f;
 
     float moveDir;
-    
+    float verticalVelocity;
+    const float GRAVITY = -90.81f;
+
     CharacterController characterController;
     InputManager inputs;
+    Player player;
 
     void Awake()
     {
         characterController = GetComponent<CharacterController>();
         inputs = GetComponentInChildren<InputManager>();
+        player = GameManager.Instance.Player;
     }
 
     void Update()
     {
         Vector3 moveVector = default;
+
         if (!inputs) // backwards compatibility after I reworked this script
         {
             moveVector.x = Input.GetAxis("Horizontal");
             moveVector.z = Input.GetAxis("Vertical");
         }
-        else
-        {
-            moveVector = new (inputs.MoveInput.x, 0, inputs.MoveInput.y);
-        }
+        else { moveVector = new Vector3(inputs.MoveInput.x, 0, inputs.MoveInput.y); }
 
         if (moveVector.magnitude > 1) moveVector.Normalize();
 
-        var player = GetComponent<Player>();
+        // Apply gravity
+        if (characterController.isGrounded)
+        {
+            verticalVelocity = 0; // Reset vertical velocity when grounded
+        }
+        else
+        {
+            verticalVelocity += GRAVITY * Time.deltaTime; // Apply gravity
+        }
+
+        moveVector.y = verticalVelocity; // Include vertical velocity in movement
+
         characterController.Move(moveVector * (speed * (player.PlayerStats[Player.Stats.MovementSpeed] / 100f) * Time.deltaTime));
     }
 }
